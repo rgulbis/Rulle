721f7109ab9a9a5a65c1a1ce8e72a4ae78611a5e
rgulbis <rgubis41@gmail.com>
2026-09-11 08:25:12 +0300

    Revert Filament SPA mode - caused a real stuck/unresponsive panel bug after login

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->spa()
             ->colors([
                 'primary' => Color::Amber,
             ])
